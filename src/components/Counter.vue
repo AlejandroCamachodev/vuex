e5d97dd0b42c537cecef563298aa5173e6cdad86
Alejandro <soloskilos@gmail.com>
2022-09-18 22:27:41 -0700

    intro-vuex-finalizado

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,44 +1,42 @@
-<template >
-    <h1>Counter- Vuex</h1>
-    <h2>Direct Access Counter: {{$store.state.count}}</h2>
-    <b-button variant="success" @click="increment">+1</b-button>
-    <b-button variant="danger" @click="incrementBy">+5</b-button>   <!--a este boton se le pasaron parametros desde la mutacion -->
-    <b-button variant="warning" @click="randomInt">Random</b-button>  
-   
+<template>
+  <h1>Counter- Vuex</h1>
+  <h2>Counter from state: {{ $store.state.counter.count }}</h2>
+  <b-button variant="success" @click="increment">+1</b-button>
+  <b-button variant="success" @click="incrementBy">+5</b-button>
+  <b-button variant="danger" :disabled="isLoading" @click="randomInt"
+    >Random</b-button
+  >
 
-    <!-- <h1>mapState</h1> -->
-    <h2>mapState: {{countComputed}}</h2>
-    <!-- <h2>Computed: {{count}}</h2> -->
-    <h2>Last mutation: {{lastMutation}}</h2>
+  <!-- <h1>mapState</h1> -->
+  <h2>mapState: {{ countComputed }}</h2>
+  <!-- <h2>Computed: {{count}}</h2> -->
+  <h2>Last mutation: {{ lastMutation }}</h2>
+</template>
 
- </template>
- 
- 
- <script>
+<script>
+import { mapState, mapActions } from "vuex";
 
-import {mapState, mapActions} from 'vuex'
-
- export default {
-
-        computed: {
-            countComputed(){
-                return this.$store.state.count
-            },
-            ...mapState(['count', 'lastMutation']),
+export default {
+  computed: {
+    countComputed() {
+      return this.$store.state.counter.count;
+    },
+    ...mapState( 'counter', ['count','lastMutation','isLoading'])
+  },
+  // forma:1  ...mapActions(['incrementRandomInt'])
+  // aqui se cambia el nombre para evitar posibles redundancias vvv
+  methods: {
+        increment() {
+            this.$store.commit('counter/increment')
         },
-        methods: {
-            increment() {
-                this.$store.commit('increment')
-            },
-            incrementBy() {
-                this.$store.commit('incrementBy', 5)
-            },
-            // forma:1  ...mapActions(['incrementRandomInt'])
-            // aqui se cambia el nombre para evitar posibles redundancias vvv
-            ...mapActions({
-                randomInt: 'incrementRandomInt'
-            })
+        incrementBy() {
+            this.$store.commit('counter/incrementBy', 5 )
+            // this.randomInt()
+        },
+        // ...mapActions( 'counter', ['incrementRandomInt'])
+        ...mapActions( 'counter', {
+            randomInt: 'incrementRandomInt'
+        })
     }
-}
- </script>
- 
+};
+</script>
